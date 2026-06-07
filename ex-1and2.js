04b75a0cfa27a3018d14bef7da559eb0f00ee338
Pachara.V <tumtum0669555@gmail.com>
2026-06-07 14:09:42 +0700

    Ex : javaScript1ST

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const countRestaurant = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+countRestaurant = 2500;
